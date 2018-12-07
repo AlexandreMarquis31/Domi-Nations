c7012e347ceea0a7fa426b581d76fa3df18a8c34
Alexandre Marquis <alexandre.marquis31@gmail.com>
2018-12-07 10:58:18 +0100

    Add Dominos

--- src/game/gameManager.java
+++ src/game/gameManager.java
@@ -5,13 +5,16 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Collections;
 
 public class gameManager {
     ArrayList<player> listPlayers = new ArrayList<player>();
     ArrayList<domino> listDominos = new ArrayList<domino>();
-
     public gameManager() {
-        String csvFile = "dominos.csv";
+        importDominos("dominos.csv");
+    }
+    public void importDominos(String dominosCSVPath){
+        String csvFile = dominosCSVPath;
         BufferedReader br = null;
         String line = "";
         String cvsSplitBy = ",";
@@ -20,10 +23,8 @@ public class gameManager {
             br.readLine();
             while ((line = br.readLine()) != null) {
                 String[] infos = line.split(cvsSplitBy);
-                System.out.println(infos[0] + "  "+infos[1] + "  " + infos[2] + "  "+ infos[3] + "  "+ infos[4] + "  ");
                 listDominos.add(new domino(new dominoPart(infos[1],Boolean.parseBoolean(infos[0])),new dominoPart(infos[3],Boolean.parseBoolean(infos[2])),  Integer.parseInt(infos[4]) ));
             }
-            System.out.println(listDominos);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -37,5 +38,9 @@ public class gameManager {
                 }
             }
         }
+    }
+    public void newGame(String rules, ArrayList<player> listPlayers){
+        Collections.shuffle(listDominos);
+        this.listPlayers = listPlayers;
     }
 }
