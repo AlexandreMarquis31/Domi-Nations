82a9b8da634abe2f887e965bcf9a57732b43efe7
Simon Talpe <simon.talpe@hotmail.fr>
2018-12-21 10:57:00 +0100

    test github simon

--- src/game/gameManager.java
+++ src/game/gameManager.java
@@ -131,4 +131,4 @@ public class gameManager {
     private Comparator<Pair<domino, player>> PairComparator = (Pair<domino, player> m1, Pair<domino, player> m2)->Integer.compare(m2.getKey().number,m1.getKey().number);
 }
 
-//Coucou
+//Coucou tu vas bien ?
